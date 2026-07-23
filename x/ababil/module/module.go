34f7b8ae69c0e61f99d3825ceca4c2b0dddb29b0
ababil-network <ababilcoin@gmail.com>
2026-07-23 13:21:46 +0800

    Add AppModule with registration functionality

--- x/ababil/module/module.go
+++ x/ababil/module/module.go
@@ -1,3 +1,13 @@
 package module
 
+import "fmt"
+
 type AppModule struct{}
+
+func (AppModule) Name() string {
+	return "ababil"
+}
+
+func (AppModule) Register() {
+	fmt.Println("ABABIL module registered")
+}
