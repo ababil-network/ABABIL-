1d41993d98464fa9efda838bef68f929dd3c29c3
ababil-network <ababilcoin@gmail.com>
2026-07-24 17:52:33 +0800

    Add production constants

---
+++ app/constants.go
@@ -0,0 +1,23 @@
+package app
+
+const (
+	// Application
+	AppName    = "ABABIL Network"
+	AppVersion = "0.1.0-dev"
+
+	// Network
+	ChainID = "ababil-devnet-1"
+
+	// Coin
+	BaseDenom    = "uabl"
+	DisplayDenom = "ABABIL"
+
+	// Binary
+	BinaryName = "ababild"
+
+	// Address Prefix
+	Bech32Prefix = "ababil"
+
+	// Block Target
+	TargetBlockTime = "1s"
+)
