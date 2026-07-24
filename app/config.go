fb9c5c3fa8e4aacb6d577ce9b08d60c7200f92cc
ababil-network <ababilcoin@gmail.com>
2026-07-24 17:56:24 +0800

    Remove duplicate constants

--- app/config.go
+++ app/config.go
@@ -1,17 +1,1 @@
 package app
-
-const (
-	Name = "ABABIL Network"
-
-	Version = "0.1.0-dev"
-
-	ChainID = "ababil-devnet-1"
-
-	Denom = "ABABIL"
-
-	Bech32Prefix = "ababil"
-
-	DefaultBondDenom = "ABABIL"
-
-	BinaryName = "ababild"
-)
